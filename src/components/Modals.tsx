c6e7443b4473b42202ba28335371e608de925479
Shevchenko Dmytro <venchesters.2002@gmail.com>
2024-09-06 01:07:58 +0300

    added functionality for creating a new channel

--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 
 import { CreateWorkspaceModal } from '@/features/workspaces/components/CreateWorkspaceModal';
+import { CreateChannelModal } from '@/features/channels/components/CreateChannelModal';
 
 export const Modals = () => {
   const [mounted, setMounted] = useState(false);
@@ -16,6 +17,7 @@ export const Modals = () => {
   return (
     <>
       <CreateWorkspaceModal />
+      <CreateChannelModal/>
     </>
   );
 };
---
+++ src/features/channels/components/CreateChannelModal.tsx
@@ -0,0 +1,80 @@
+import { toast } from 'sonner';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/Dialog';
+import { Input } from '@/components/ui/Input';
+import { Button } from '@/components/ui/Button';
+import { useWorkspaceId } from '@/hooks/useWorkspaceId';
+
+import { useCreateChannel } from '../api/useCreateChannel';
+import { useCreateChannelModal } from '../store/useCreateChannelModal';
+
+export const CreateChannelModal = () => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+
+  const { mutate, isPending } = useCreateChannel();
+  const [open, setOpen] = useCreateChannelModal();
+
+  const [name, setName] = useState('');
+
+  const handleClose = () => {
+    setName('');
+    setOpen(false);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, '-').toLowerCase();
+    setName(value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate(
+      { name, workspaceId },
+      {
+        onSuccess: (id) => {
+          toast.success('Channel created');
+          router.push(`/workspace/${workspaceId}/channel/${id}`);
+          handleClose();
+        },
+        onError: () => {
+          toast.error('Failed to create channel');
+        }
+      },
+    );
+  };
+  
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a channel</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            disabled={isPending}
+            onChange={handleChange}
+            required
+            autoFocus
+            minLength={3}
+            maxLength={80}
+            placeholder="e.g. plan-budget"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>
+              Create
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
